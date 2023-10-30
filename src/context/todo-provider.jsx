518f7d075b4f8eeff7c5f0ff0fa3bdd26c90cce7
Hiskia Tamba <hiskiatamba23@gmail.com>
2023-10-30 23:29:38 +0700

    first commit

---
+++ src/context/todo-provider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+function TodoProvider({ children }) {
+  const [todos, setTodos] = useState([
+    { id: 1, value: "Membuat website sekolah", status: false },
+    { id: 2, value: "Memperbaiki Laptop", status: false },
+    { id: 3, value: "Belajar Ngoding", status: false },
+    { id: 3, value: "Belajar React JS", status: false },
+  ]);
+  const [todoInput, setTodoInput] = useState("");
+  const [todoEdit, setTodoEdit] = useState({});
+
+  const [isEdit, setIsEdit] = useState("");
+
+  return (
+    <TodoContext.Provider value={{ todos, setTodos, todoInput, setTodoInput, isEdit, setIsEdit, todoEdit, setTodoEdit }}>
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export default TodoProvider;
